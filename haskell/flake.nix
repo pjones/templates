6dc58ab5be8792418e64a34c5aaa407399d35258
Peter J. Jones <peter@jonesbunch.com>
2026-02-17 17:18:58 +0100

    haskell: Update dependencies

--- haskell/flake.nix
+++ haskell/flake.nix
@@ -8,7 +8,8 @@
     haskellrc.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, ... }@inputs:
+  outputs =
+    { self, nixpkgs, ... }@inputs:
     let
       # The name of the Haskell package:
       packageName = "hello";
@@ -20,56 +21,58 @@
 
       # List of supported compilers:
       supportedCompilers = [
-        "ghc8107"
-        "ghc902"
-        "ghc923"
+        "ghc98"
+        "ghc910"
+        "ghc912"
       ];
 
       # List of supported systems:
       supportedSystems = [ "x86_64-linux" ];
 
       # Function to generate a set based on supported systems:
-      forAllSystems = f:
-        nixpkgs.lib.genAttrs supportedSystems (system: f system);
+      forAllSystems = f: nixpkgs.lib.genAttrs supportedSystems (system: f system);
 
       # Attribute set of nixpkgs for each system:
       nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; });
 
       # A source file list cleaner for Haskell programs:
-      haskellSourceFilter = src:
+      haskellSourceFilter =
+        src:
         nixpkgs.lib.cleanSourceWith {
           inherit src;
-          filter = name: type:
-            let baseName = baseNameOf (toString name); in
-            nixpkgs.lib.cleanSourceFilter name type &&
-            !(
-              baseName == "dist-newstyle"
-              || nixpkgs.lib.hasPrefix "." baseName
-            );
+          filter =
+            name: type:
+            let
+              baseName = baseNameOf (toString name);
+            in
+            nixpkgs.lib.cleanSourceFilter name type
+            && !(baseName == "dist-newstyle" || nixpkgs.lib.hasPrefix "." baseName);
         };
 
       # The package derivation:
-      derivation = haskell:
-        haskell.callCabal2nix
-          packageName
-          (haskellSourceFilter ./.)
-          (packageOverrides haskell);
+      derivation =
+        haskell: haskell.callCabal2nix packageName (haskellSourceFilter ./.) (packageOverrides haskell);
     in
     {
-      packages = forAllSystems (system:
-        let pkgs = nixpkgsFor.${system}; in
+      packages = forAllSystems (
+        system:
+        let
+          pkgs = nixpkgsFor.${system};
+        in
         {
           # The full Haskell package for the default compiler:
           ${packageName} = derivation pkgs.haskellPackages;
 
           # Just the executables for the default compiler:
           default = pkgs.haskell.lib.justStaticExecutables (derivation pkgs.haskellPackages);
-        } // builtins.listToAttrs (map
-          (compiler: {
+        }
+        // builtins.listToAttrs (
+          map (compiler: {
             name = "${packageName}-${compiler}";
             value = derivation pkgs.haskell.packages.${compiler};
-          })
-          supportedCompilers));
+          }) supportedCompilers
+        )
+      );
 
       devShells = forAllSystems (system: {
         default = nixpkgsFor.${system}.haskellPackages.shellFor {
