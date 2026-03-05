7e8899adaf36ecfe923a2d6270b86d9f0ea36779
Peter J. Jones <peter@jonesbunch.com>
2026-03-05 15:10:15 +0100

    ipynb: Add additional dependencies

--- ipynb/flake.nix
+++ ipynb/flake.nix
@@ -6,7 +6,7 @@
   };
 
   outputs =
-    { nixpkgs, ... }:
+    { self, nixpkgs, ... }:
     let
       supportedSystems = [
         "x86_64-linux"
@@ -27,14 +27,47 @@
 
     in
     {
+      packages = each (
+        pkgs: system: {
+          dtreeviz = pkgs.callPackage (
+            {
+              python3Packages,
+              fetchPypi,
+            }:
+            python3Packages.buildPythonPackage rec {
+              version = "2.3.2";
+              pname = "dtreeviz";
+              format = "pyproject";
+              src = fetchPypi {
+                inherit pname version;
+                hash = "sha256-2EE6ATcyNxUPzMt2PkaThPvQVaYnUX1327AZChQD8Fk=";
+              };
+              build-system = with python3Packages; [
+                setuptools
+              ];
+              dependencies = with python3Packages; [
+                colour
+                graphviz
+                matplotlib
+                numpy
+                pandas
+                scikit-learn
+              ];
+            }
+          ) { };
+        }
+      );
+
       devShells = each (
         pkgs: system: {
           default = pkgs.mkShell {
             buildInputs = [
               pkgs.basedpyright
+              pkgs.graphviz
 
               (pkgs.python3.withPackages (
                 pypkgs: with pypkgs; [
+                  biopython
                   black
                   jupyter
                   matplotlib
@@ -42,8 +75,11 @@
                   numpy
                   openpyxl
                   pandas
-                  seaborn
+                  reportlab
                   scikit-learn
+                  seaborn
+                  self.packages.${system}.dtreeviz
+                  statsmodels
                   tabulate
                 ]
               ))
